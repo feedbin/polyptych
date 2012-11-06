0e68904d78084d0531987b1985883b9b4e4b0f2f
Ben Ubois <ben@benubois.com>
2012-11-06 06:43:08 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Polyptych::Application.config.secret_token = 'adc85aac7ebbc00a28740c5d924739e5e7ff816a0dd89dc458bbea67680c4cf01bf21ae543f161a656f91c7840615f14f418a6ea61693e418f7994661c1969c1'
