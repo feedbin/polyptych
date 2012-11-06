6618a35733cbacda5c2f220a359e54d1f83160a8
Ben Ubois <ben@benubois.com>
2012-11-06 12:17:52 -0800

    Ruby 1.9 syntax.

--- app/helpers/panels_helper.rb
+++ app/helpers/panels_helper.rb
@@ -1,6 +1,6 @@
 module PanelsHelper
   def compress_css(css)
-    compressed_css = Sass::Engine.new(css, :syntax => :scss, :style => :compressed); 
+    compressed_css = Sass::Engine.new(css, syntax: :scss, style: :compressed); 
     compressed_css.render
   end
 end
