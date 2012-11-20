2b40d5fc9328ea24f3a01051a03f61f2fcbf7fa8
Ben Ubois <ben@benubois.com>
2012-11-20 00:11:45 -0800

    Turn off rack cache.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,9 @@ Polyptych::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
+  # Turn off rack cache
+  config.action_dispatch.rack_cache = nil
+
+  
 end
