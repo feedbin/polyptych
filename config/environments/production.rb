250d87b650ac13611705cfea0071d9488530e8ee
Ben Ubois <ben@benubois.com>
2012-11-22 13:51:39 -0800

    Turning on Rack::Cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Polyptych::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
   
-  middleware.insert_before('Rack::Lock', Rack::Deflater)
+  middleware.insert_before('Rack::Cache', Rack::Deflater)
 
   # Code is not reloaded between requests
   config.cache_classes = true
@@ -66,9 +66,5 @@ Polyptych::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-  
-  # Turn off rack cache
-  config.action_dispatch.rack_cache = nil
-
   
 end
